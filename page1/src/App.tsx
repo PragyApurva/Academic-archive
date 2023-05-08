8e3137a7c83c4aba67c212db66565af0809ec71d
HardKofi <2020mcb1229@iitrpr.ac.in>
2023-05-08 19:29:11 +0530

    MERGED PAGES

--- page1/src/App.tsx
+++ page1/src/App.tsx
@@ -3,7 +3,11 @@ import type { FC } from 'react';
 
 import classes from './App.module.css';
 import resets from './components/_resets.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 import { LandingPage } from './components/LandingPage/LandingPage';
+import { Footer } from './components/Footer/Footer';
+import { Page2 } from './components/Page2/Page2';
+import { Page3 } from './components/Page3/Page3';
 
 interface Props {
   className?: string;
@@ -11,7 +15,13 @@ interface Props {
 export const App: FC<Props> = memo(function App(props = {}) {
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
-      <LandingPage />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <Navbar />
+        <LandingPage />
+        <Page2 />
+        <Page3 />
+        <Footer />
+      </div>
     </div>
   );
 });
